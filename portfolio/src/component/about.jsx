95ae984d48f5de5691b54f9fe1852ca49ce83a4b
Uddeshy Manoj Jaiswal <uddeshkrrish@gmail.com>
2023-03-18 13:01:24 +0530

    newchanges

--- portfolio/src/component/about.jsx
+++ portfolio/src/component/about.jsx
@@ -11,7 +11,7 @@ const About = () => {
             <Box className="image-section" width={["80%", "75%", "50%", "30%"]} margin="auto" marginTop="4%" borderRadius="50%">
                 <Image src="https://ca.slack-edge.com/T044M5T44JV-U045JEVURMH-960522c0f125-512" className="img-fluid home-img" borderRadius="50%" />
             </Box>
-            <Box className="about-section" width={["95%", "98%", "50%", "50%"]} margin="auto" textAlign="center">
+            <Box className="about-section" width={["95%", "98%", "50%", "50%"]} margin="auto" textAlign="center" id="user-detail-intro">
                 <Heading as="h1"> A Full Stack MERN Developer</Heading>
                 <br />
                 <Text>A Full Stack web developer with more than 1200+ hours of experience of coding and creating dynamic and responsive web applications. I have a strong background in both front-end and back-end development, as well as experience working with thechnologies like React, Redux, Api, . I'm always interested in learning new technologies and staying up-to-date on the latest trends in web development.</Text>
--- portfolio/src/component/contact.jsx
+++ portfolio/src/component/contact.jsx
@@ -1,67 +1,83 @@
-import React from "react";
+import React, { useRef } from "react";
 import { GrLinkedin } from "react-icons/gr"
 import { GoMarkGithub } from "react-icons/go"
 import { SiMinutemailer } from "react-icons/si"
 import { ImWhatsapp } from "react-icons/im"
 import emailjs from 'emailjs-com';
-import { Box, Heading, HStack } from "@chakra-ui/react";
+import { Box, FormLabel, Heading, HStack, Input, useToast } from "@chakra-ui/react";
 const Contect = () => {
+    const form = useRef();
+    const toast = useToast()
     function sendEmail(e) {
         e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
 
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+        emailjs.sendForm('service_oagozmk', 'template_qjqhwtx', e.target, 'OuzZBMHHoETuNVlA-')
             .then((result) => {
-                
+                toast({
+                    title: 'Message sent .',
+                    description: "Message has been sent succecfully.",
+                    status: 'success',
+                    duration: 3000,
+                    isClosable: true,
+                })
             }, (error) => {
-                console.log(error.text);
+                toast({
+                    title: 'Message Failed.',
+                    description: `There was some ${error.text}.`,
+                    status: 'error',
+                    duration: 3000,
+                    isClosable: true,
+                })
             });
     }
 
     return (
-        <Box className="section contact-section nav-link contact" paddingTop={["15%","15%","12%","3%"]} id="contact">
-          <Heading as="h1" fontSize={["3xl","3xl","4xl","4xl"]}>Contacts</Heading>
-          <Box marginTop="20px">
-            <h1 className="h4" id="contact-phone">Phone No : +91 8808346600</h1>
-            <HStack className="contact-child" width={["100%","95%","70%","50%"]} height={["250px","250px","300px","300px"]}>
-                <Box id="contact-github">
-                    <a href="https://github.com/Elizah7" target="next-page">
-                        <div>
-                            <GoMarkGithub size="50%"/>
-                        </div>
-                        <h4>Github</h4>
-                    </a>
-                </Box>
-                <Box id="contact-linkedin">
-                    <a href="https://www.linkedin.com/in/uddeshy-jaiswal-56a69a201/" target="next-page">
-                        <div>
-                            <GrLinkedin size="50%" />
-                        </div>
-                        <h4>Linkedin</h4>
-                    </a>
-                </Box>
-                <Box>
-                    <a href="https://wa.me/918808346600" target="_blank">
-                        <div>
-                            <ImWhatsapp size="50%"/>
-                        </div>
-                        <h4>Whataspp</h4>
-                    </a>
-                </Box>
-            </HStack>
+        <Box className="section contact-section nav-link contact" paddingTop={["15%", "15%", "12%", "3%"]} id="contact" height={["150vh","150vh","200vh","200vh"]}>
+            <Heading as="h1" fontSize={["3xl", "3xl", "4xl", "4xl"]}>Contacts</Heading>
+            <Box marginTop="20px">
+                <h1 className="h4" id="contact-phone">Phone No : +91 8808346600</h1>
+                <HStack className="contact-child" width={["100%", "95%", "70%", "50%"]} height={["250px", "250px", "300px", "300px"]}>
+                    <Box id="contact-github">
+                        <a href="https://github.com/Elizah7" target="next-page">
+                            <div>
+                                <GoMarkGithub size="50%" />
+                            </div>
+                            <h4>Github</h4>
+                        </a>
+                    </Box>
+                    <Box id="contact-linkedin">
+                        <a href="https://www.linkedin.com/in/uddeshy-jaiswal-56a69a201/" target="next-page">
+                            <div>
+                                <GrLinkedin size="50%" />
+                            </div>
+                            <h4>Linkedin</h4>
+                        </a>
+                    </Box>
+                    <Box>
+                        <a href="https://wa.me/918808346600" target="_blank">
+                            <div>
+                                <ImWhatsapp size="50%" />
+                            </div>
+                            <h4>Whataspp</h4>
+                        </a>
+                    </Box>
+                </HStack>
             </Box>
-            {/* <form className="contact-form" onSubmit={sendEmail}>
-                <input type="hidden" name="contact_number" />
-                <label>Name</label>
-                <input type="text" name="from_name" />
-                <label>Email</label>
-                <input type="email" name="from_email" />
-                <label>Subject</label>
-                <input type="text" name="subject" />
-                <label>Message</label>
-                <textarea name="html_message" />
-                <input type="submit" value="Send" />
-            </form> */}
-  
+            <Box width={["100%","100%","50%","50%"]} className="form" margin="auto" marginTop="50px" id="contact-email">
+                <Heading as="h1">Email</Heading>
+            <form ref={form} onSubmit={sendEmail}>
+                <FormLabel>Name</FormLabel>
+                <Input type="text" name="user_name" />
+                <FormLabel>Email</FormLabel>
+                <Input type="email" name="user_email" />
+                <FormLabel>Subject</FormLabel>
+                <Input type="text" name="subject" />
+                <FormLabel>Message</FormLabel>
+                <textarea name="message" />
+                <Input type="submit" value="Send"  className="from_submit"/>
+            </form>
+            </Box>
+            <Heading as="h4">Thanks for visiting</Heading>
         </Box>
 
     )
