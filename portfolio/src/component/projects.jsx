9ad2d3d23391079578693020d28ca7eee2bc548e
Uddeshy Manoj Jaiswal <uddeshkrrish@gmail.com>
2023-08-17 10:39:01 +0530

    updatedtheprojectsurl

--- portfolio/src/component/projects.jsx
+++ portfolio/src/component/projects.jsx
@@ -29,11 +29,11 @@ const Projects = () => {
     { img: "style/industrybying6.png" },
   ]
   const gullible = [
-    {img:"style/gullible.png"},
-    {img:"style/gullible2.png"},
-    {img:"style/gullible3.png"},
-    {img:"style/gullible4.png"},
-    {img:"style/gullible5.png"}
+    { img: "https://user-images.githubusercontent.com/112868723/238127717-e3d4d9f2-7eb6-4f5f-8b19-c54994c6eeca.png" },
+    { img: "https://user-images.githubusercontent.com/112868723/238127803-a99817a5-f8bb-422a-a6bc-228553062bf7.png" },
+    { img: "https://user-images.githubusercontent.com/112868723/238127822-91500ce6-36de-40bb-9b8b-a9c470a7eb3a.png" },
+    { img: "https://user-images.githubusercontent.com/112868723/238127842-85efccec-63ea-4dca-9e5b-732f1a3b1923.png" },
+    { img: "https://user-images.githubusercontent.com/112868723/238128024-de86f3f9-527d-4e2a-9c98-fd937b413a8d.png" }
   ]
   const divStyle = {
     display: 'flex',
@@ -48,6 +48,45 @@ const Projects = () => {
       <Box className="projects-section section nav-link projects" paddingTop={["15%", "15%", "12%", "3%"]} fontSize={["xm", "sm", "md", "l"]} id="projects" >
         <Heading as="h1" fontSize={["3xl", "3xl", "4xl", "4xl"]}>My Projects</Heading>
         <Box className="projects-container">
+          <Box data-aos="fade-up" display={{ base: "flex", sm: "flex", md: "flex" }} flexDirection={["column-reverse", "column-reverse", "row", "row"]} className="project-div project-card" height={["550px", "460px", "430px", "450px"]} width={["100%", "100%", "95%", "95%"]}>
+            <Box className="textl" width={["100%", "100%", "50%", "50%"]} textAlign={["left", "left", "center", "center"]} height={["60%", "60%", "100%", "100%"]} >
+              <Box display="flex" justifyContent="space-around">
+                <Heading as="h2" className="project-title">Name: Daily-Essentials</Heading>
+                <Heading as="h2">Ful-Stack Project</Heading>
+              </Box>
+              <Box marginTop={["0", "0", "15px", "50px"]} className="project-description">
+                <p>This is a clone of E-Commerse website Daily-Objects this was a group project we were five team members and we built this website in four days, my part was to build the landing page and backend  </p>
+              </Box>
+              <Box marginTop={["10px", "10px", "15px", "50px"]} >
+                <Heading as="h2">Tech Stack :</Heading>
+                <Box marginTop={["10px", "10px", "15px", "50px"]} className="tech_stack">
+                <Heading as="h6">React</Heading>
+                  <Heading as="h6">Chakra-Ui</Heading>
+                  <Heading as="h6">Api</Heading>
+                  <Heading as="h6">Redux</Heading>
+                  <Heading as="h6">Express</Heading>
+                  <Heading as="h6">NodeJs</Heading>
+                  <Heading as="h6">MongoDb</Heading>
+                </Box>
+              </Box>
+              <Box marginTop={["10px", "10px", "60px", "50px"]} className="live-button">
+                <a href="https://github.com/KoushikNiyogi/Daily-objects-clone" target="next-page" className="project-github-link">Github</a>
+                <a href="https://daily-essential-koushikniyogi.vercel.app" target="next-page" className="project-deployed-link">Live server</a>
+              </Box  >
+            </Box>
+            <Box className="slide-container" width={["100%", "100%", "50%", "50%"]} height={["40%", "40%", "100%", "99%"]}>
+              <Slide>
+                {gullible.map((slideImage, index) => (
+                  <Box key={index} height={["220px", "220px", "430px", "448px"]}>
+                    <Box display="flex" justifyContent="center" height="100%">
+                      <Image src={slideImage.img} />
+                    </Box>
+
+                  </Box>
+                ))}
+              </Slide>
+            </Box>
+          </Box>
           <Box data-aos="fade-up" display={{ base: "block", sm: "block", md: "flex" }} className="project-div project-card" height={["550px", "460px", "430px", "450px"]} width={["100%", "100%", "95%", "95%"]}>
             <Box className="slide-container" width={["100%", "100%", "50%", "50%"]} height={["40%", "40%", "100%", "99%"]}>
               <Slide>
@@ -84,8 +123,8 @@ const Projects = () => {
               </Box>
             </Box>
           </Box>
-          <Box data-aos="fade-up" display={{ base: "flex", sm: "flex", md: "flex" }} flexDirection={["column-reverse","column-reverse","row","row"]} className="project-div project-card" height={["550px", "460px", "430px", "450px"]} width={["100%", "100%", "95%", "95%"]}>
-            <Box className="textl" width={["100%", "100%", "50%", "50%"]} textAlign={["left", "left", "center", "center"]} height={["60%","60%","100%","100%"]} >
+          <Box data-aos="fade-up" display={{ base: "flex", sm: "flex", md: "flex" }} flexDirection={["column-reverse", "column-reverse", "row", "row"]} className="project-div project-card" height={["550px", "460px", "430px", "450px"]} width={["100%", "100%", "95%", "95%"]}>
+            <Box className="textl" width={["100%", "100%", "50%", "50%"]} textAlign={["left", "left", "center", "center"]} height={["60%", "60%", "100%", "100%"]} >
               <Box display="flex" justifyContent="space-around">
                 <Heading as="h2" className="project-title">Name: Fashion Mantra</Heading>
                 <Heading as="h2">Front-End Project</Heading>
@@ -160,45 +199,10 @@ const Projects = () => {
               </Box>
             </Box>
           </Box>
-          <Box data-aos="fade-up" display={{ base: "flex", sm: "flex", md: "flex" }} flexDirection={["column-reverse","column-reverse","row","row"]} className="project-div project-card" height={["550px", "460px", "430px", "450px"]} width={["100%", "100%", "95%", "95%"]}>
-            <Box className="textl" width={["100%", "100%", "50%", "50%"]} textAlign={["left", "left", "center", "center"]} height={["60%","60%","100%","100%"]} >
-              <Box display="flex" justifyContent="space-around">
-                <Heading as="h2" className="project-title">Name: Asos</Heading>
-                <Heading as="h2">Front-End Project</Heading>
-              </Box>
-              <Box marginTop={["0", "0", "15px", "50px"]} className="project-description">
-                <p>This is a clone of E-Commerse website Asos, this was a group project we were five team members and we built this website in four days, my part was to build the cart section  </p>
-              </Box>
-              <Box marginTop={["10px", "10px", "15px", "50px"]} >
-                <Heading as="h2">Tech Stack :</Heading>
-                <Box marginTop={["10px", "10px", "15px", "50px"]} className="tech_stack">
-                  <Heading as="h6">Html</Heading>
-                  <Heading as="h6">CSS</Heading>
-                  <Heading as="h6">Javascript</Heading>
-                </Box>
-              </Box>
-              <Box marginTop={["10px", "10px", "60px", "50px"]} className="live-button">
-                <a href="https://github.com/Elizah7/gullible-pear-795" target="next-page" className="project-github-link">Github</a>
-                <a href="https://gullible-pear-795.vercel.app/" target="next-page" className="project-deployed-link">Live server</a>
-              </Box  >
-            </Box>
-            <Box className="slide-container" width={["100%", "100%", "50%", "50%"]} height={["40%", "40%", "100%", "99%"]}>
-              <Slide>
-                {gullible.map((slideImage, index) => (
-                  <Box key={index} height={["220px", "220px", "430px", "448px"]}>
-                    <Box display="flex" justifyContent="center" height="100%">
-                      <Image src={slideImage.img} />
-                    </Box>
-
-                  </Box>
-                ))}
-              </Slide>
-            </Box>
-          </Box>
         </Box>
 
       </Box>
-      <Box className="github-section" marginTop={["5%","5%","5%","5%"]}>
+      <Box className="github-section" marginTop={["5%", "5%", "5%", "5%"]}>
         <Calender />
       </Box>
 
